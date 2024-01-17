761c91fc20d7e39f86a0392f7de9f88869aa8b7a
Vinoth Kumar Ponnusamy <vinothkumarp911@gmail.com>
2024-01-17 15:52:02 +0100

    day2 - part 2 done

--- go/day2/main.go
+++ go/day2/main.go
@@ -8,12 +8,16 @@ import (
 )
 
 type game struct {
-	gameIDNumber, minRed, minGreen, minBlue, maxRed, maxGreen, maxBlue, totalReds, totalGreens, totalBlues int
-	rounds                                                                                                 []string
+	gameIDNumber, maxRed, maxGreen, maxBlue, totalReds, totalGreens, totalBlues int
+	rounds                                                                      []string
 }
 
 func (g game) numOfRounds() int {
 	return len(g.rounds)
+}
+
+func (g game) gameScore() int {
+	return g.maxRed * g.maxGreen * g.maxBlue
 }
 
 func main() {
@@ -23,22 +27,27 @@ func main() {
 	// TODO: Squeeze parsed values into struct
 	var games []game //?
 
-	for num, line := range fileLines {
-		gameIDNumber, maxRed, maxGreen, maxBlue, reds, greens, blues, roundScores := ParseGameDetails(line)
-		games = append(games, game{gameIDNumber, maxRed, maxGreen, maxBlue, reds, greens, blues, roundScores})
-		fmt.Println("Game ", gameIDNumber, ":")
-		fmt.Println(games[num])
+	for _, line := range fileLines {
+		game := ParseGameDetails(line)
+		games = append(games, game)
+		// fmt.Println("Game ", game.gameIDNumber, ":")
+		// fmt.Println("Total number of rounds:", game.numOfRounds())
+		// fmt.Println(games[num])
 	}
 
-	// Solution to Part - 1
-	// Sum of Game IDs where games possible with 12 red, 13 green, and 14 blue cubes
+	// Solution to Part - 1 & 2
+	// Part 1 - Sum of Game IDs where games possible with 12 red, 13 green, and 14 blue cubes
+	// Part 2 - Sum of power of games, where power is the product of the max value drawn for each color in a game
 	var sumOfGameIDs int
+	var sumOfGamePowers int
 	for _, game := range games {
 		if game.maxRed <= 12 && game.maxGreen <= 13 && game.maxBlue <= 14 {
 			sumOfGameIDs += game.gameIDNumber
 		}
+		sumOfGamePowers += game.gameScore()
 	}
 	fmt.Println("Solution to Part 1 = ", sumOfGameIDs)
+	fmt.Println("Solution to Part 2 = ", sumOfGamePowers)
 }
 
 func getGameIDNumber(GameID string) string {
@@ -46,19 +55,8 @@ func getGameIDNumber(GameID string) string {
 	return indexSplit[len(indexSplit)-1]
 }
 
-func ParseGameDetails(GameDetailsLine string) (int, int, int, int, int, int, int, []string) {
-	var GameID int
-	var Rounds []string
-	var MinRed int
-	var MinGreen int
-	var MinBlue int
-	var MaxRed int
-	var MaxGreen int
-	var MaxBlue int
-	var TotalReds int
-	var TotalGreens int
-	var TotalBlues int
-	// var GameDetails game
+func ParseGameDetails(GameDetailsLine string) game {
+	var Game game
 
 	gameIDMarker, gameScores, found := strings.Cut(GameDetailsLine, ":")
 
@@ -68,45 +66,36 @@ func ParseGameDetails(GameDetailsLine string) (int, int, int, int, int, int, int
 		if err != nil {
 			fmt.Println(err)
 		} else {
-			GameID = GameIDInt
+			Game.gameIDNumber = GameIDInt
 		}
 
-		Rounds = strings.Split(gameScores, ";")
-		fmt.Println("Rounds :")
-		for _, round := range Rounds {
-			fmt.Println(round)
+		Game.rounds = strings.Split(gameScores, ";")
+		// fmt.Println("Rounds :")
+		for _, round := range Game.rounds {
+			// fmt.Println(round)
 			red, green, blue := splitByColor(round)
 
 			// Find Highest Value drawn in game for each color
-			if red > MaxRed {
-				MaxRed = red
+			if red > Game.maxRed {
+				Game.maxRed = red
 			}
-			if green > MaxGreen {
-				MaxGreen = green
+			if green > Game.maxGreen {
+				Game.maxGreen = green
 			}
-			if blue > MaxBlue {
-				MaxBlue = blue
-			}
-			// Find Lowest Value drawn in game for each color
-			if red < MinRed {
-				MinRed = red
-			}
-			if green < MinGreen {
-				MinGreen = green
-			}
-			if blue < MinBlue {
-				MinBlue = blue
+			if blue > Game.maxBlue {
+				Game.maxBlue = blue
 			}
 
 			// Find cumlative value of drawn cubes in game for each color
-			TotalReds += red
-			TotalGreens += green
-			TotalBlues += blue
+			Game.totalReds += red
+			Game.totalGreens += green
+			Game.totalBlues += blue
 		}
 	} else {
 		fmt.Println("Input file seems to be corrupted. Empty lines found.")
 	}
-	return GameID, MaxRed, MaxGreen, MaxBlue, TotalReds, TotalGreens, TotalBlues, Rounds
+
+	return Game
 }
 
 func splitByColor(RoundDetails string) (int, int, int) {
